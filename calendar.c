1e08cbaf5b61a6a7ae9107ac6a11b0659927c1f3
Sleepe005 <kholopov005@mail.ru>
2024-10-13 21:23:34 +0300

    new pareser, good input and output

--- calendar.c
+++ calendar.c
@@ -11,108 +11,62 @@ int cursePos = 0;
 int maxCursePos = 8;
 
 // Воод 1-ого времени
-char strTime1[9];
+char strTime1[10] = "00:00:00";
 bool hasStrTime1 = false;
 
 // Ввод 1-ой даты
-char strDate1[11];
+char strDate1[12] = "01.01.1930";
 bool hasStrDate1 = false;
 
+// Дата и время 1
 unsigned long long int date1 = 0;
 
 // Ввод второго времени
-char strTime2[9];
+char strTime2[10] = "00:00:00";
 bool hasStrTime2 = false;
 
 // Ввод второй даты
-char strDate2[11];
+char strDate2[12] = "01.01.1930";
 bool hasStrDate2 = false;
 
+// Дата и время 2
 unsigned long long int date2 = 0;
 
 // Ввод кол-ва дней
 char addDays[5];
 bool hasAddDays = false;
 
-unsigned long long addDaysInSecond = 0;
-
 // Ввод времени
 char addTime[9];
 bool hasAddTime = false;
-
-unsigned long long int addTimeInSeconds = 0;
+unsigned long long int addTimeSec = 0;
 
 // Result 5
 unsigned long long int totalSecondsRes5 = 0;
 bool hasRes5 = false;
 int res5[5];
 
+// Default value
 int startDate[3] = {1,1,1930};
 int startTime[3] = {0,0,0};
 
-// Function for printing menu
-void printMenu(){
-    char menu[9][75] = {
-        "1. Ввод 1-ого времени",
-        "2. Ввод 1-ой даты",
-        "3. Ввод 2-ого времени",
-        "4. Ввод 2-ой даты",
-        "5. Результат (большая дата - меньшая дата)",
-        "6. Ввод кол-ва дней",
-        "7. Результат (Дата (1) + Кол-во дней (6))",
-        "8. Ввод времени",
-        "9. Результат (Время (1) + Время (8))"
-        };
-
-    for(int i = 0; i < 9; ++i){
-        printw("%s", menu[i]);
-        if(hasStrTime1 && i == 0){
-            printw(": %s", strTime1);
-        }
-        if(hasStrDate1 && i == 1){
-            printw(": %s", strDate1);
-        }
-        if(hasStrTime2 && i == 2){
-            printw(": %s", strTime2);
-        }
-        if(hasStrDate2 && i == 3){
-            printw(": %s", strDate2);
-        }
-        if(hasAddDays && i == 5){
-            printw(": %s", addDays);
-        }
-        if(hasAddTime && i == 7){
-            printw(": %s", addTime);
-        }
-        if (i == cursePos){
-            printw("  %s", "<--");
-        }
-        printw("\n");
-    }
-}
-
-// Parse time in string format to array
-void timeStrParser(char strData[9], int *valueTime){ // 23:59:27
-    char substr[3] = "";
-
-    for(int i = 0, j = 0; i < 8, j < 3; i += 3, j++){
-        strncpy(substr, strData+i, 2);
-        valueTime[j] = atoi(substr);
-    }
-}
-
-// Parse date in string format to array
-void dateStrParser(char strData[11], int *valueData){ // 01.01.2020
-    char substr[5] = "";
-
-    for(int i = 0, j = 0; i < 8, j < 3; i += 3, j++){
-        if(j == 2){
-            strncpy(substr, strData+i, 4);
+// Parse data
+void Split(char data[12], int *value){
+    char element[5] = "";
+    int pos = 0;
+    int j = 0;
+    for(int i = 0; i < strlen(data); ++i){
+        if(ispunct(data[i])){
+            value[j] = atoi(element);
+            j++;
+            strcpy(element, "");
+            pos = 0;
         }else{
-            strncpy(substr, strData+i, 2);
+            element[pos] = data[i];
+            pos++;
         }
-        valueData[j] = atoi(substr);
     }
+    value[j] = atoi(element);
 }
 
 // Function witch define visokos year
@@ -134,8 +88,9 @@ int valDayInMonth(int year, int month){
     bool vis = visokosYear(year);
 
     int res;
+    month%=12;
 
-    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 0)
     {
         res = 31;
     }
@@ -157,29 +112,28 @@ int valDayInMonth(int year, int month){
 }
 
 // Coder for date in seconds
-unsigned long long int codeDateTimeToSeconds(char strData[11], char strTime[9]){
+unsigned long long int codeDateTimeToSeconds(char strData[12], char strTime[10]){
     unsigned long long int totalSeconds = 0;
 
     int date[3] = {0,0,0};
-    dateStrParser(strData, date);
+    Split(strData, date);
 
     int time[3] = {0,0,0};
-    timeStrParser(strTime, time);
+    Split(strTime, time);
 
     for(int i = startDate[2]; i < date[2]; ++i){
         if(visokosYear(i)){
-            totalSeconds += 366;
+            totalSeconds += 366*86400;
         }else{
-            totalSeconds += 365;
+            totalSeconds += 365*86400;
         }
     }
 
     for(int i = 1; i < date[1]; ++i){
-        totalSeconds += (unsigned long long int)valDayInMonth(date[2], i);
+        totalSeconds += (unsigned long long int)(valDayInMonth(date[2], i)*86400);
     }
 
-    totalSeconds += (unsigned long long int)(date[0] - 1);
-    totalSeconds *= 86400;
+    totalSeconds += (unsigned long long int)((date[0] - 1)*86400);
     
     totalSeconds += (unsigned long long int)(time[0]*60*60+time[1]*60 + time[2]);
 
@@ -233,40 +187,95 @@ int decodeDateTimeFromSeconds(unsigned long long int totalSeconds, int *dateTime
 
     return totalDays;
 }
+
+// Check input format
+bool isSample(char strData[12]){
+    bool allDigIsDig = true;
+    bool hasSep = true; // __.__.____/__:__:__
+    for(int i = 0; i < strlen(strData); ++i){
+        if(i == 2 || i == 5){
+            if(!ispunct(strData[i])){
+                hasSep = false;
+            }
+        }else if(!isdigit(strData[i]))
+            allDigIsDig = false;
+        }
+    return allDigIsDig && hasSep;
+}
+
 // Do something
 void doSomething(int doing){
     switch (doing)
     {
     case 0:
         clear();
-        printw("Введите первое время: ");
+        printw("Введите первое время (ЧЧ:ММ:СС): ");
         scanw("%s", strTime1);
-        // TODO: Ввод
+
         hasStrTime1 = true;
+        if(!isSample(strTime1)){
+            strcpy(strTime1, "");
+            hasStrTime1 = false;
+            printf("\nНеверный формат времени :(\n");
+            printf("\nНажмите любую клавишу для выхода в меню :(\n");
+            getch();
+        }else{
+            date1 += codeDateTimeToSeconds("01.01.1930", strTime1);
+        }
+
         break;
     
     case 1:
         clear();
-        printw("Введите первую дату время: ");
+        printw("Введите первую дату (ДД:ММ:ГГГГ): ");
         scanw("%s", strDate1);
-        // TODO: Ввод
+        
         hasStrDate1 = true;
+        if(!isSample(strDate1)){
+            strcpy(strDate1, "");
+            hasStrDate1 = false;
+            printf("\nНеверный формат даты :(\n");
+            printf("\nНажмите любую клавишу для выхода в меню :(\n");
+            getch();
+        }else{
+            date1 += codeDateTimeToSeconds(strDate1, "00:00:00");
+        }
+
         break;
     
     case 2:
         clear();
-        printw("Введите второе время: ");
+        printw("Введите второе время (ЧЧ:ММ:СС): ");
         scanw("%s", strTime2);
-        // TODO: Ввод
+
         hasStrTime2 = true;
+        if(!isSample(strTime2)){
+            strcpy(strTime2, "");
+            hasStrTime2 = false;
+            printf("\nНеверный формат времени :(\n");
+            printf("\nНажмите любую клавишу для выхода в меню :(\n");
+            getch();
+        }else{
+            date2 += codeDateTimeToSeconds("01.01.1930", strTime2);
+        }
+
         break;
     
     case 3:
         clear();
-        printw("Введите вторую дату: ");
+        printw("Введите вторую дату (ДД:ММ:ГГГГ): ");
         scanw("%s", strDate2);
-        // TODO: Ввод
+
         hasStrDate2 = true;
+        if(!isSample(strDate2)){
+            strcpy(strDate2, "");
+            hasStrDate2 = false;
+            printf("\nНеверный формат даты :(\n");
+            printf("\nНажмите любую клавишу для выхода в меню :(\n");
+            getch();
+        }else{
+            date1 += codeDateTimeToSeconds(strDate2, "00:00:00");
+        }
         break;
     
     case 4:
@@ -297,13 +306,21 @@ void doSomething(int doing){
         getch();
         break;
 
-
     case 5:
         clear();
         printw("Введите кол-во дней: ");
         scanw("%s", &addDays);
-        // TODO: Ввод
         hasAddDays = true;
+        for(int i = 0; i < strlen(addDays); ++i){
+            if(!isdigit(addDays[i])){
+                hasAddDays = false;
+            }
+        }
+        if(!hasAddDays){
+            printf("\nНеверный формат :(\n");
+            printf("\nНажмите любую клавишу для выхода в меню :(\n");
+            getch();
+        }
         break;
     
     case 6:
@@ -312,12 +329,15 @@ void doSomething(int doing){
             unsigned long long int totalSec1 = codeDateTimeToSeconds(strDate1, strTime1);
             unsigned long long int totalSecAdd = atoi(addDays)*24*3600;
 
-            int dateTimeAdd[6];
+            int dateTimeAdd[6] = {0,0,0,0,0,0};
             unsigned long long int totalSecRes = totalSec1 + totalSecAdd;
             decodeDateTimeFromSeconds(totalSecRes, dateTimeAdd, startDate);
 
-            printw("Первая дата: %s\n", strDate1);
-            printw("Новая дата: %d.%d.%d\n", dateTimeAdd[5], dateTimeAdd[4], dateTimeAdd[3]);
+            int d1[6]  = {0,0,0,0,0,0};
+            decodeDateTimeFromSeconds(totalSec1, d1, startDate);
+
+            printw("Первая дата: %02d.%02d.%d\n", d1[5], d1[4], d1[3]);
+            printw("Новая дата: %02d.%02d.%d\n", dateTimeAdd[5], dateTimeAdd[4], dateTimeAdd[3]);
         }else{
             printw("Некоторые данные не были введены\n");
         }
@@ -328,10 +348,20 @@ void doSomething(int doing){
     
     case 7:
         clear();
-        printw("Введите время: ");
+        printw("Введите время (ЧЧ:ММ:СС): ");
         scanw("%s", &addTime);
-        // TODO: Ввод
+
         hasAddTime = true;
+        if(!isSample(addTime)){
+            strcpy(addTime, "");
+            hasAddTime = false;
+            printf("\nНеверный формат времени :(\n");
+            printf("\nНажмите любую клавишу для выхода в меню :(\n");
+            getch();
+        }else{
+            addTimeSec += codeDateTimeToSeconds("01.01.1930", addTime);
+        }
+
         break;
     
     case 8:
@@ -344,8 +374,11 @@ void doSomething(int doing){
             unsigned long long int totalSecRes = totalSec1 + totalSecAdd;
             decodeDateTimeFromSeconds(totalSecRes, dateTimeAdd, startDate);
 
-            printw("Первое время: %s\n", strTime1);
-            printw("Новое время: %d.%d.%d\n", dateTimeAdd[0], dateTimeAdd[1], dateTimeAdd[2]);
+            int time1[6] = {0,0,0,0,0,0};
+            decodeDateTimeFromSeconds(totalSec1, time1, startDate);
+
+            printw("Первое время: %02d:%02d:%02d\n", time1[0], time1[1], time1[2]);
+            printw("Новое время: %02d:%02d:%02d\n", dateTimeAdd[0], dateTimeAdd[1], dateTimeAdd[2]);
         }else{
             printw("Некоторые данные не были введены\n");
         }
@@ -356,6 +389,54 @@ void doSomething(int doing){
 
     default:
         break;
+    }
+}
+
+// Function for printing menu
+void printMenu(){
+
+    char menu[9][75] = {
+        "1. Ввод 1-ого времени",
+        "2. Ввод 1-ой даты",
+        "3. Ввод 2-ого времени",
+        "4. Ввод 2-ой даты",
+        "5. Результат (большая дата - меньшая дата)",
+        "6. Ввод кол-ва дней",
+        "7. Результат (Дата (1) + Кол-во дней (6))",
+        "8. Ввод времени",
+        "9. Результат (Время (1) + Время (8))"
+        };
+
+    int valDate1[6] = {0,0,0,0,0,0};
+    decodeDateTimeFromSeconds(date1, valDate1, startDate);
+
+    int valDate2[6] = {0,0,0,0,0,0};
+    decodeDateTimeFromSeconds(date2, valDate2, startDate);
+
+    for(int i = 0; i < 9; ++i){
+        printw("%s", menu[i]);
+        if(hasStrTime1 && i == 0){
+            printw(": %02d:%02d:%02d", valDate1[0], valDate1[1], valDate1[2]);
+        }
+        if(hasStrDate1 && i == 1){
+            printw(": %02d.%02d.%d", valDate1[5], valDate1[4], valDate1[3]);
+        }
+        if(hasStrTime2 && i == 2){
+            printw(": %02d:%02d:%02d", valDate2[0], valDate2[1], valDate2[2]);
+        }
+        if(hasStrDate2 && i == 3){
+            printw(": %02d.%02d.%d", valDate2[5], valDate2[4], valDate2[3]);
+        }
+        if(hasAddDays && i == 5){
+            printw(": %s", addDays);
+        }
+        if(hasAddTime && i == 7){
+            printw(": %s", addTime);
+        }
+        if (i == cursePos){
+            printw("  %s", "<--");
+        }
+        printw("\n");
     }
 }
 
